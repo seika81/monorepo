dfeab9e1870acdb07aaf9f8ce4ce270123e77e52
池星華 <ikeseika@ikeseikanoMacBook-Air-2.local>
2025-01-13 21:37:26 +0900

    update sbconfig

--- apps/app-1/.storybook/main.ts
+++ apps/app-1/.storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
 
 import { join, dirname } from "path";
 
@@ -11,6 +12,17 @@ function getAbsolutePath(value: string): any {
 }
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.plugins = [
+        ...(config.resolve.plugins || []),
+        new TsconfigPathsPlugin({
+          extensions: config.resolve.extensions,
+        }),
+      ];
+    }
+    return config;
+  },
   addons: [
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-essentials"),
