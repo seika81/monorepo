9a0bd739ddd42d63f999811143d0b669d54ea637
池星華 <ikeseika@ikeseikanoMacBook-Air-2.local>
2025-02-05 11:34:45 +0900

    update ui

--- packages/ui/src/component/Price/index.tsx
+++ packages/ui/src/component/Price/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
 
 export const Price: FC<{ value: number }> = ({ value }) => {
-  return <h3 style={{ color: "blue" }}>¥{value}</h3>;
+  return <h3 style={{ color: "black" }}>¥{value}</h3>;
 };
