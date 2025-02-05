b12fbf80fa864fdfaa259e3fa931a04224cc08f3
池星華 <ikeseika@ikeseikanoMacBook-Air-2.local>
2025-02-05 18:52:40 +0900

    update

--- apps/app-1/src/component/InputPrice/index.tsx
+++ apps/app-1/src/component/InputPrice/index.tsx
@@ -8,7 +8,7 @@ export const InputPrice = () => {
   const handleOnChange = (val: string) => setPrice(Number(val));
   return (
     <div>
-      oossss
+      oosssss
       <input
         value={price}
         onChange={(event) => handleOnChange(event.target.value)}
